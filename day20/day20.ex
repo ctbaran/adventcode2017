9e56b0a583fcfe6f52e323bf95569373c72b88c5
Christian Baran <christian@wealthsimple.com>
2017-12-21 00:14:44 -0500

    Day 20 part 1

---
+++ day20/day20.ex
@@ -0,0 +1,35 @@
+defmodule Day20 do
+
+  def solve(input) do
+    File.stream!(input)
+    |> Stream.map(&String.strip/1)
+    |> Stream.with_index
+    |> Stream.map(&to_particle/1)
+    |> Enum.to_list
+    |> Stream.iterate(&update_particles/1)
+    |> Enum.at(10000)
+    |> Enum.min_by(fn {_, {pos, _, _}} -> manhattan_distance(pos) end)
+  end
+
+  def to_particle({string, num}) do
+    [_, x_p,y_p,z_p,x_v,y_v,z_v,x_a,y_a,z_a] = Regex.run(~r/p=<(-?\d+),(-?\d+),(-?\d+)>, v=<(-?\d+),(-?\d+),(-?\d+)>, a=<(-?\d+),(-?\d+),(-?\d+)>/, string)
+    {num, {to_i(x_p,y_p,z_p), to_i(x_v,y_v,z_v), to_i(x_a,y_a,z_a)}}
+  end
+
+  def manhattan_distance({x,y,z}) do
+    abs(x) + abs(y) + abs(z)
+  end
+
+  def to_i(x,y,z), do: {String.to_integer(x),String.to_integer(y),String.to_integer(z)}
+
+  def update_particles(particles) do
+    Enum.map(particles, &update_particle/1)
+  end
+
+  def update_particle({num, {{xp, yp, zp}, {xv, yv, zv}, {xa, ya, za}}}) do
+    {nxv, nyv, nzv} = {xv + xa, yv + ya, zv + za}
+    {nxp, nyp, nzp}  = {xp + nxv, yp + nyv, zp + nzv}
+    {num, {{nxp, nyp, nzp}, {nxv, nyv, nzv}, {xa, ya, za}}}
+  end
+end
+
